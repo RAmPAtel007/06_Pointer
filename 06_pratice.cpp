64b0bc5c0d462e149f7c56479f1d23f7738ab0b9
RAmPAtel007 <codewizard0007@gmail.com>
2024-12-26 19:34:56 +0530

    pointers from notes

---
+++ 06_pratice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a=7;
+    // int b=17;
+    // int *c =&b;
+    // *c=7;
+    // cout<<a<<" "<<b<<endl;
+
+//     char arr[20];
+//    int i;
+//    for(i=0;i<0;i++){
+//     *(arr+i)= 65 + i;
+//    }
+//    *(arr + i) ='\0';
+//    cout<< arr;
+//    return 0;
+}
